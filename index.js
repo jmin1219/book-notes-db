1773c160bb363173db03f3c953aba0a38be2826c
Jaymin <jmin1219@gmail.com>
2024-11-06 14:41:27 +0900

    finalize ejs files and request routes; create-form HTML

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import bodyParser from "body-parser";
 import express from "express";
 import pg from "pg";
@@ -8,6 +9,7 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// POSTGRESQL DATABASE
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
@@ -15,12 +17,36 @@ const db = new pg.Client({
   password: "PGDtthtp!1",
   port: 5432,
 });
-
 db.connect();
 
+// BOOK COVER API ACCESS
+const API_KEY = "";
+const API_URL = "";
+
+// GET HOMEPAGE ie. LIBRARY CARD-VIEW
 app.get("/", (req, res) => {
   res.render("library.ejs");
 });
+
+// GET RECENT POSTS LIST (like D Sivers website)
+app.get("/posts-list", (req, res) => {});
+
+// GET CREATE POST FORM
+app.get("/new-post", (req, res) => {
+  res.render("create-form.ejs");
+});
+
+// GET BOOK POST
+app.get("/post/:id", (req, res) => {});
+
+// EDIT BOOK POST
+app.post("/edit-post/:id", (req, res) => {});
+
+// SUBMIT BOOK POST
+app.post("/submit-post", (req, res) => {});
+
+// DELETE BOOK POST
+app.post("/delete-post/:id", (req, res) => {});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
